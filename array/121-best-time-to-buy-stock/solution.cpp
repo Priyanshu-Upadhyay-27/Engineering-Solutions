868cd51d2be2c6332715d51688424e4065208b8a
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-02-09 23:47:06 +0530

    Solved best time to buy a stock

---
+++ array/121-best-time-to-buy-stock/solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+int main() {
+    Solution sol;
+    int maximum_profit;
+
+    vector<int> prices = {7,1,5,3,6,4};
+
+    maximum_profit = sol.maxProfit(prices);
+
+    cout<<"Maximum Profit : "<<maximum_profit;
+}
