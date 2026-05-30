c4c6d29bf1ed97d625454514d418579fd8d5f5ce
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-30 23:59:19 +0530

    Completed the main function

--- recursion/Sort-a-stack/solution.cpp
+++ recursion/Sort-a-stack/solution.cpp
@@ -1,11 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void insertRight(stack<int> st, int el){
-    
+void insertRight(stack<int> &st, int el){
+    if(st.empty() || st.top() < el){
+        st.push(el);
+        return;
+    }
+    int t = st.top();
+    st.pop();
+    insertRight(st, el);
+    st.push(t);
 }
 
-void sortStack(stack<int>unsortedStack){
+void sortStack(stack<int> &unsortedStack){
     if(unsortedStack.empty()) return;
     
     int temp = unsortedStack.top();
@@ -15,5 +22,30 @@ void sortStack(stack<int>unsortedStack){
 }
 
 int main(){
+    stack<int> st;
+    st.push(30);
+    st.push(10);
+    st.push(50);
+    st.push(20);
+    st.push(40);
+
+    cout << "Original Stack (Top to Bottom): ";
+    stack<int> temp = st;
+    while(!temp.empty()){
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    cout << endl;
+
+    sortStack(st);
+
+    cout << "Sorted Stack (Top to Bottom): ";
+    while(!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+
+    return 0;
     
 }
