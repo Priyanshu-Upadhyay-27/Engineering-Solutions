4dd92d06c92563a0906f54927cecdb38e877d5a4
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-06-01 00:07:02 +0530

    Solved TOH using recursion

--- recursion/Tower-of-Hanoi/solution.cpp
+++ recursion/Tower-of-Hanoi/solution.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve(int s, int h, int d, int n, int& count){
-    
+void solve(int s, int d, int h, int n, int& count){
+    if(n == 1){
+        cout<< "Step " << count << ":"<<"Moving "<<n<<" plates from tower "<<s<<" to tower "<<d<<endl;
+        return;
+    }
+    solve(s, h, d, n - 1, count);
+    count++;
+    cout<< "Step " << count << ":"<<"Moving "<<n<<" plates from tower "<<s<<" to tower "<<d<<endl;
+    solve(h, d, s, n - 1, count);
+}
+
+int main(){
+    int s = 1, h = 2, d = 3, n, count = 0;
+    cout<<"Enter the number of plates :"<<endl;
+    cin>>n;
+    solve(s, d, h, n, count);
+    cout << "\nTotal moves required: " << count << endl;
 }
