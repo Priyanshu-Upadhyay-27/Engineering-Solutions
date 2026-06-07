232043522168c65bafaf66c1a9ea0eb8d08e8af4
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-06-08 00:20:49 +0530

    Solved permutation spaces

---
+++ recursion/permutation-spaces-av/solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(string ip, string op){
+    if(ip.size() == 0){
+        cout<<op<<endl;
+        return;
+    }
+    string op1 = op;
+    string op2 = op;
+    op1.push_back(ip[0]);
+    op2.push_back(' ');
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin());
+    solve(ip, op1);
+    solve(ip, op2);
+}
+
+int main(){
+    string ip;
+    cout<<"Enter an input string: "<<endl;
+    cin>>ip;
+    string op = "";
+    op.push_back(ip[0]);
+    ip.erase(ip.begin() + 0);
+    solve(ip, op);
+}
