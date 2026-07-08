1bd82be4b229cf0da085198073eec79160f4418b
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-07-09 00:41:53 +0530

    added solution

---
+++ array/1796-second-largest-element-from-alphanumeric-string/README.md
@@ -0,0 +1,16 @@
+# 1796 Second Largest Digit in a String
+
+**Link:** [LeetCode](https://leetcode.com/problems/second-largest-digit-in-a-string/description/)  
+**Difficulty:** Easy
+
+## Approach
+Brief one-line approach description.
+
+
+---
+
+**Time Complexity:** O(?)  
+**Space Complexity:** O(?)
+
+## Key Learning
+Main takeaway or important concept from this problem.
---
+++ array/1796-second-largest-element-from-alphanumeric-string/solution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int secondHighest(string s) {
+        int first = -1;
+        int second = -1;
+        for (auto c : s) {
+            if (isdigit(c)) {
+                int digit = c - '0';
+                if (digit > first) {
+                    second = first;
+                    first = digit;
+                }
+                else if (digit < first && digit > second) {
+                    second = digit;
+                }
+            }
+        }
+        return second;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    
+
+    return 0;
+}
