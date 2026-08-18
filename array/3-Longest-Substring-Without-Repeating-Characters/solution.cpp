e86da4719d6d532440d9c51c21b2e2d7875d5327
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-08-18 11:29:30 +0530

    Solved the question

---
+++ array/3-Longest-Substring-Without-Repeating-Characters/solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i = 0;
+        int n = s.size();
+        int maxLen = 0;
+        unordered_map<char, int> lastIndex;
+        for(int j = 0; j < n; j++){
+            char c = s[j];
+
+            if (lastIndex.count(c) && lastIndex[c] >= i) {
+                i = lastIndex[c] + 1;
+            }
+            lastIndex[c] = j;
+            maxLen = max(maxLen, j - i + 1);
+        }
+        return maxLen;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s1 = "";
+}
